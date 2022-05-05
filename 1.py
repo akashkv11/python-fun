03b437543e77b035de89d130dc1eec363fd444a9
Akash K V <95403802+akashvinod11@users.noreply.github.com>
2022-05-06 00:24:44 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,8 @@
+import pyautogui as pg
+import time 
+
+for j in range (500):
+    pg.write("Hello ")
+    pg.press('enter')
+    time.sleep(2)
+        
---
+++ sp.py
@@ -0,0 +1,7 @@
+import pyautogui as pg
+import time 
+time.sleep(50)
+
+for i in range(20):
+    pg.write("Myre")
+    pg.press('enter')
